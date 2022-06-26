ce511af879a3c916c12186d3713d07916794b54b
favy-codez <107569845+favy-codez@users.noreply.github.com>
2022-06-27 06:52:33 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// favours part starts here
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:8,
+    nav:false,
+    autoplay:true,
+    dots:false,
+    arrows:false,
+    responsive:{
+        1000:{
+            items:3,
+            nav:false,
+        }
+    }
+})
+//favours part ends here
+ 
