63e627869b8c6ee9236219536b8cce137b2cc1b3
unknown <devfranklin5@gmail.com>
2022-06-29 11:54:02 +0100

    hero-section add

--- script.js
+++ script.js
@@ -14,4 +14,20 @@ $('.owl-carousel').owlCarousel({
     }
 })
 //favours part ends here
- 
+ //Franklin Parts starts here
+ const hero = document.querySelector('.hero-left');
+const heroL = document.querySelector('.hero-right');
+gsap.from(hero,{
+    duration: "2.5",
+    ease:"power2.out",
+    opacity: 0,
+    x:-500
+})
+
+gsap.from(heroL,{
+    duration: "2.5",
+    ease:"power2.out",
+    x:500,
+    opacity:0
+})
+ //Franklin Parts ends here
